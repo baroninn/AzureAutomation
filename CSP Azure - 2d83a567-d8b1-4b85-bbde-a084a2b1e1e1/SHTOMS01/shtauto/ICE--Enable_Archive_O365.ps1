b46c033f685089b19f3ce77fb48384e219ec6dce
shareonline <jakob@shareonline.dk>
2016-11-22 15:01:19 +0100

    Groupbased Archiving
    ICE_O365_Archive_1Year

--- CSP Azure - 2d83a567-d8b1-4b85-bbde-a084a2b1e1e1/SHTOMS01/shtauto/ICE--Enable_Archive_O365.ps1
+++ CSP Azure - 2d83a567-d8b1-4b85-bbde-a084a2b1e1e1/SHTOMS01/shtauto/ICE--Enable_Archive_O365.ps1
@@ -10,23 +10,27 @@ try{
     Import-Module (Import-PSSession -Session $Session `
                                     -AllowClobber `
                                     -DisableNameChecking `
-                                    -CommandName Get-Mailbox, Enable-Mailbox, Set-Mailbox) -Global
+                                    -CommandName Get-Mailbox, Enable-Mailbox, Set-Mailbox, Get-Group) -Global
 
     }catch{
            Write-Output "failed exchange connection.."
 }
 
+$members = Get-Group ICE_O365_Archive_1Year | select members
+
 if($session){
 
-    $mailbox = Get-Mailbox -Filter {ArchiveStatus -eq "None" -and RecipientTypeDetails -eq "UserMailbox"}
+    foreach($member in $members.Members){
+        $mailbox = Get-Mailbox "$member" -Filter {ArchiveStatus -eq "None" -and RecipientTypeDetails -eq "UserMailbox"}
 
-    foreach($i in $mailbox){
+        foreach($i in $mailbox){
 
-            Enable-Mailbox -Identity $i.Identity -Archive -ArchiveName ($i.alias + " - Archive")
-            Set-Mailbox -Identity $i.Identity -RetentionPolicy "2 year archive"
-            Write-output "Archive and retention enabled for $i"
-            
-            }
+                Enable-Mailbox -Identity $i.Identity -Archive -ArchiveName ($i.alias + " - Archive")
+                Set-Mailbox -Identity $i.Identity -RetentionPolicy "1 year archive"
+                Write-output "Archive and retention enabled for $i"
+                }
+        }
 }
 
 Get-PSSession | Remove-PSSession
+
